c36a6acaf662ba1b79eadf1a3b0dfe64d5fed8f4
mantrabrain <mantrabrain@gmail.com>
2020-04-20 20:47:37 +0545

    booking & enquiry form template update

--- templates/parts/tour-sidebar.php
+++ templates/parts/tour-sidebar.php
@@ -1,5 +1,45 @@
 <div class="yatra-tour-info">
     <div class="yatra-tour-info-inner">
-        <?php yatra_frontend_options(); ?>
+        <h2>Tour Options</h2>
+        <div class="yatra-tabs">
+
+            <ul class="yatra-tab-wrap" role="tablist">
+                <li class="item active" role="presentation"><a href="#yatra-tour-booking-form" role="tab" tabindex="0"
+                                                               aria-controls="yatra-tour-booking-form"
+                                                               data-aria-selected="true">
+                        Booking Form</a></li>
+                <li class="item" role="presentation"><a href="#yatra-enquiry-form" role="tab" tabindex="-1"
+                                                        aria-controls="yatra-enquiry-form">
+                        Enquiry Form</a></li>
+
+            </ul>
+            <section id="yatra-tour-booking-form" class="yatra-tab-content" role="tabpanel">
+                <div class="tab-inner" tabindex="0">
+                    <h3 class="tab-title">Overview</h3>
+                    <div class="yatra-tour-booking-form-section">
+                        <div class="sec-row row">
+                        </div><!-- .sec-row -->
+                    </div>
+                </div>
+            </section>
+            <section id="yatra-enquiry-form" class="yatra-tab-content" role="tabpanel" aria-hidden="true">
+                <div class="tab-inner" tabindex="0">
+                    <h3 class="tab-title">Cost Info</h3>
+
+                    <div class="cost-info-half cost_included">
+                        <h4>Price includes</h4>
+                    </div>
+
+                    <div class="cost-info-half cost_excluded">
+                        <h4>Price excludes</h4>
+                    </div>
+
+
+                </div>
+            </section>
+
+
+        </div>
+        <?php //yatra_frontend_options(); ?>
     </div>
 </div>
