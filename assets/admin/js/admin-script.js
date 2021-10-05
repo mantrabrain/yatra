b2348ca0ed37d3eb8de87922fe29e5a11eab7b7e
Umesh Ghimire <me.umeshuser@gmail.com>
2021-10-05 15:28:45 +0545

    update minor issue

--- assets/admin/js/admin-script.js
+++ assets/admin/js/admin-script.js
@@ -730,15 +730,17 @@
 			var label_name = wrap.find('ul').attr('data-label-name');
 			var uuid = this.getUniqueID('text_');
 			var li = $('<li data-tab-type="' + uuid + '"/>');
-
-			//label
-			li.append('<input class="yatra_frontend_tabs_available_options_label" name="' + this.replaceAll(label_name, uuid) + '" type="text" value="Tab Label"/>');
 			//name
-			li.append('<span class="label">Tab Label Goes Here</span><input class="yatra_frontend_tabs_available_options_icon icopick" name="' + this.replaceAll(icon_name, uuid) + '" type="text"/>');
+			li.append('<span class="label">Tab Label Goes Here</span><input class="yatra_frontend_tabs_available_options_label" name="' + this.replaceAll(label_name, uuid) + '" type="text" value="Tab Label"/>');
+			//label
+			li.append('<input class="yatra_frontend_tabs_available_options_icon icopick" name="' + this.replaceAll(icon_name, uuid) + '" type="text"/>');
 			//icon
 			li.append('<input name="' + this.replaceAll(type_name, uuid) + '" type="hidden" value="text"/>');
+
 			li.append('<span><button type="button" class="available-tab-remove-item">x</button></span>');
+
 			wrap.find('ul').append(li);
+
 			this.updateTabOrdering();
 
 		},
