bb93b64e3f1c42980c37e6d4a429c6efced091b3
Umesh Ghimire <me.umeshuser@gmail.com>
2021-10-21 18:03:32 +0545

    DB managed

--- includes/classes/class-yatra-core-db.php
+++ includes/classes/class-yatra-core-db.php
@@ -125,33 +125,4 @@ class Yatra_Core_DB
         echo '</pre>';
     }
 
-
-    public function course_item_id($course_id, $user_id)
-    {
-
-        global $wpdb;
-
-        $sql = $wpdb->prepare(
-            "SELECT user_item_id from " . SIKSHYA_DB_PREFIX . 'user_items ui
-            INNER JOIN ' . $wpdb->prefix . 'posts p ON p.ID=ui.reference_id
-
-            WHERE ui.user_id=%d and ui.parent_id=0 and  ui.reference_type=%s and ui.status=%s and ui.item_id=%d and p.post_type=%s',
-            $user_id,
-            SIKSHYA_ORDERS_CUSTOM_POST_TYPE,
-            'enrolled',
-            $course_id,
-            SIKSHYA_ORDERS_CUSTOM_POST_TYPE
-
-
-        );
-
-        $results = $wpdb->get_col($sql);
-
-
-        if ($results[0]) {
-            return $results[0];
-        }
-        return 0;
-
-    }
 }
