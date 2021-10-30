b511950faf7b53da83495a3fda8bdf4964c15a3a
ughimire <me.umeshuser@gmail.com>
2021-10-30 21:40:46 +0545

    update

--- templates/tour/additional.php
+++ templates/tour/additional.php
@@ -1,26 +1,50 @@
+<?php
+echo '<pre>';
+print_r($additional_info);
+echo '</pre>';
+?>
 <div class="yatra-tour-additional-info">
     <div class="yatra-tour-additional-info-item">
-        <i class="icon fa fa-user"></i><span
-                class="info-title"><?php echo esc_html__('Price Per:', 'yatra'); ?>
-            <strong><?php echo esc_html($additional_info['pricing_per']); ?></strong></span>
+        <div class="icon-wrap">
+            <i class="icon fa fa-user-clock"></i>
+        </div>
+        <div class="icon-content">
+            <span class="info-title"><?php echo esc_html__('Duration', 'yatra'); ?></span>
+            <p class="info-content"><?php echo esc_html($additional_info['tour_duration']); ?></p>
+        </div>
     </div>
     <div class="yatra-tour-additional-info-item">
-        <i class="icon fa fa-user-circle"></i><span class="info-title"><?php echo esc_html__('Group Size:', 'yatra'); ?>
-            <strong><?php echo esc_html($additional_info['group_size']); ?></strong></span>
+        <div class="icon-wrap">
+            <i class="icon fa fa-clock"></i>
+        </div>
+        <div class="icon-content">
+            <span class="info-title"><?php echo esc_html__('Country', 'yatra'); ?></span>
+            <p class="info-content"><?php echo esc_html($additional_info['country']); ?></p>
+        </div>
     </div>
     <div class="yatra-tour-additional-info-item">
-        <i class="icon fa fa-clock"></i><span class="info-title"><?php echo esc_html__('Duration:', 'yatra'); ?>
-            <strong><?php echo esc_html($additional_info['tour_duration']); ?></strong></span>
+        <div class="icon-wrap">
+            <i class="icon fa fa-clock"></i>
+        </div>
+        <div class="icon-content">
+            <span class="info-title"><?php echo esc_html__('Country', 'yatra'); ?></span>
+            <p class="info-content"><?php echo esc_html($additional_info['country']); ?></p>
+        </div>
     </div>
     <div class="yatra-tour-additional-info-item">
-        <i class="icon fa fa-map"></i><span class="info-title"><?php echo esc_html__('Country:', 'yatra'); ?>
-            <strong><?php echo esc_html($additional_info['country']); ?></strong></span>
+        <div class="icon-wrap">
+            <i class="icon fa fa-clock"></i>
+        </div>
+        <div class="icon-content">
+            <span class="info-title"><?php echo esc_html__('Country', 'yatra'); ?></span>
+            <p class="info-content"><?php echo esc_html($additional_info['country']); ?></p>
+        </div>
     </div>
 
 </div>
 <?php if (yatra_tour_has_attributes()) { ?>
     <div class="yatra-tour-attribute-info">
         <?php
-        yatra_tour_custom_attributes_template(true); ?>
+        yatra_tour_custom_attributes_template(); ?>
     </div>
 <?php } ?>
