82900d186d4bae6c3eaa62c6ceba7a68b2250c37
ughimire <me.umeshuser@gmail.com>
2021-10-31 21:29:27 +0545

    date added on order table

--- templates/tmpl-order-table.php
+++ templates/tmpl-order-table.php
@@ -4,6 +4,7 @@
         <thead>
         <tr>
             <th class="tour-name"><?php echo __('Tour', 'yatra') ?></th>
+            <th class="tour-name"><?php echo __('Date', 'yatra') ?></th>
             <th class="tour-total"><?php echo __('Total', 'yatra') ?></th>
         </tr>
         </thead>
@@ -21,6 +22,11 @@
                     echo '<a target="_blank" href="' . esc_url(get_post_permalink($tour->ID)) . '" class="tour-title">' . esc_html($tour->post_title) . '</a>';
                     ?>
                 </td>
+                <td><span>
+                        <?php
+                        echo esc_html($item['selected_date']);
+                        ?>
+                    </span></td>
                 <td class="tour-total">
                     <span class="yatra-Price-amount amount"><span
                                 class="yatra-Price-currencySymbol"><?php echo yatra_get_current_currency_symbol() ?></span><?php echo $item['tour_final_price']; ?></span>
@@ -32,15 +38,20 @@
 
         <tr class="cart-subtotal">
             <th><?php echo __('Subtotal', 'yatra') ?></th>
-            <td><span class="yatra-Price-amount amount"><span
+            <td></td>
+            <td colspan="2"><span class="yatra-Price-amount amount"><span
                             class="yatra-Price-currencySymbol"><?php echo yatra_get_current_currency_symbol() ?></span><?php echo $total_price; ?></span>
             </td>
+
+
         </tr>
         <tr class="tour-book-total">
             <th><?php echo __('Total', 'yatra') ?></th>
-            <td><strong><span class="yatra-Price-amount amount"><span
+            <td></td>
+            <td colspan="2"><strong><span class="yatra-Price-amount amount"><span
                                 class="yatra-Price-currencySymbol"><?php echo yatra_get_current_currency_symbol() ?></span><?php echo $total_price; ?></span></strong>
             </td>
+
         </tr>
 
 
