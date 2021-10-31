8d273f4b8ccafd94840a347410d1f3a296ef462f
Umesh Ghimire <me.umeshuser@gmail.com>
2021-10-31 14:39:35 +0545

    update

--- templates/tour/attributes-text.php
+++ templates/tour/attributes-text.php
@@ -3,10 +3,11 @@
         <span class="attribute-icon fa fa-home"></span>
     </div>-->
     <div class="icon-content">
+        <span class="attribute-icon fa fa-pie-chart"></span>
         <?php if ('' != $title) { ?>
             <span class="attribute-title"><?php echo esc_html($title) ?></span>
         <?php } ?>
-        <span class="attribute-icon fa fa-pie-chart"></span>
+
         <p class="attribute-content"><?php echo esc_html($content) ?></p>
     </div>
 </div>
