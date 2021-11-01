cef15447ad1ffe78f640b3e71e59981c6f551d61
Umesh Ghimire <me.umeshuser@gmail.com>
2021-11-01 13:18:33 +0545

    update with pricing string

--- templates/tmpl-order-table.php
+++ templates/tmpl-order-table.php
@@ -36,8 +36,7 @@
                     </span>
                 </td>
                 <td class="tour-total">
-                    <span class="yatra-Price-amount amount"><span
-                                class="yatra-Price-currencySymbol"><?php echo yatra_get_current_currency_symbol() ?></span><?php echo $item['tour_final_price']; ?></span>
+                    <span class="yatra-Price-amount amount"><?php echo yatra_get_price(yatra_get_current_currency_symbol(), $item['tour_final_price']) ?></span>
                 </td>
             </tr>
         <?php } ?>
@@ -47,8 +46,8 @@
         <tr class="cart-subtotal">
             <th colspan="3"><?php echo __('Subtotal', 'yatra') ?></th>
 
-            <td><span class="yatra-Price-amount amount"><span
-                            class="yatra-Price-currencySymbol"><?php echo yatra_get_current_currency_symbol() ?></span><?php echo $total_price; ?></span>
+            <td><span class="yatra-Price-amount amount">
+                    <?php echo yatra_get_price(yatra_get_current_currency_symbol(), $total_price) ?></span>
             </td>
 
 
@@ -56,8 +55,9 @@
         <tr class="tour-book-total">
             <th colspan="3"><?php echo __('Total', 'yatra') ?></th>
 
-            <td><strong><span class="yatra-Price-amount amount"><span
-                                class="yatra-Price-currencySymbol"><?php echo yatra_get_current_currency_symbol() ?></span><?php echo $total_price; ?></span></strong>
+            <td><strong><span
+                            class="yatra-Price-amount amount"><?php echo yatra_get_price(yatra_get_current_currency_symbol(), $total_price) ?></span>
+                </strong>
             </td>
 
         </tr>
