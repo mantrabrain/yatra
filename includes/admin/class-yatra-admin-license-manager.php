62cad614ad580aac2b5e9db5d77e58c214782ad1
Umesh Ghimire <me.umeshuser@gmail.com>
2021-11-07 11:34:17 +0545

    license manager done

--- includes/admin/class-yatra-admin-license-manager.php
+++ includes/admin/class-yatra-admin-license-manager.php
@@ -40,9 +40,32 @@ class Yatra_Admin_License_Manager
      */
     public function license_page()
     {
+        $premium_addons = $this->premium_addons();
+
+        $message = '';
+
+        if (isset($_POST['yatra_license_save_button'])) {
+
+            $nonce_value = isset($_POST['_wpnonce']) ? sanitize_text_field($_POST['_wpnonce']) : '';
+
+            if (wp_verify_nonce($nonce_value, 'yatra_license_save_nonce')) {
+
+                $message = __('License updated. Please check license status and notice for more details.', 'yatra');
+
+                foreach ($premium_addons as $addon_slug => $addon_config) {
+
+                    $license = isset($_POST[$addon_slug . '_license']) ? sanitize_text_field($_POST[$addon_slug . '_license']) : '';
+
+                    $status = apply_filters($addon_slug . '_update', $license);
+                }
+            }
+
+
+        }
+
         echo '<div class="wrap yatra-license-page-wrap">';
 
-        yatra_load_admin_template('license.license', array('addons' => $this->premium_addons()));
+        yatra_load_admin_template('license.license', array('addons' => $premium_addons, 'message' => $message));
 
         echo '</div>';
     }
@@ -55,12 +78,12 @@ class Yatra_Admin_License_Manager
 
 
         wp_enqueue_style('yatra-license-style', YATRA_PLUGIN_URI . '/assets/admin/css/license.css', array(), YATRA_VERSION);
-        wp_enqueue_script('yatra-license-script', YATRA_PLUGIN_URI . '/assets/admin/js/license.js', array('jquery'), YATRA_VERSION);
+        /*wp_enqueue_script('yatra-license-script', YATRA_PLUGIN_URI . '/assets/admin/js/license.js', array('jquery'), YATRA_VERSION);
         $data =
             array(
                 'ajax_url' => admin_url('admin-ajax.php'),
             );
-        wp_localize_script('yatra-license-script', 'yatraLicenseScript', $data);
+        wp_localize_script('yatra-license-script', 'yatraLicenseScript', $data);*/
 
     }
 }
--- includes/admin/templates/license/license.php
+++ includes/admin/templates/license/license.php
@@ -1,38 +1,79 @@
-<table>
-    <thead>
-    <tr>
-        <th>Product Name</th>
-        <th>Product License</th>
-        <th>Expire Date</th>
-        <th>Status</th>
-    </tr>
-    </thead>
-    <tbody>
-    <?php foreach ($addons as $addon_slug => $addon) {
+<h1><?php echo __('License Manager', 'yatra') ?></h1>
 
-        $license_sample = array(
-            'yatra-services' => array(
-                'expire_date' => '2021-10-22',
-                'license_key' => 'kslkdkakasdkfasdlkfaksdfasdfasdf',
-                'status' => 'Active'
-            ));
-        $license = get_option('yatra_license', $license_sample);
-
-        $addon_license = isset($license_sample[$addon_slug]) ? $license_sample[$addon_slug] : array();
-
-        ?>
+<?php if ($message != '') { ?>
+    <div id="message" class="updated notice notice-success "><p><?php echo esc_html($message); ?></p>
+        <button type="button" class="notice-dismiss"><span class="screen-reader-text">Dismiss this notice.</span>
+        </button>
+    </div>
+<?php } ?>
+<form method="post" class="yatra-license-manager-form">
+    <table class="yatra-license-manager-table">
+        <thead>
         <tr>
-            <td><?php echo esc_html($addon['label']) ?></td>
-            <td><?php echo isset($addon_license['license_key']) ? esc_html($addon_license['license_key']) : '' ?></td>
-            <td><?php echo isset($addon_license['expire_date']) ? esc_html($addon_license['expire_date']) : '' ?></td>
-            <td><?php echo isset($addon_license['status']) ? esc_html($addon_license['status']) : '' ?></td>
-
+            <th>Product Name</th>
+            <th>Product License</th>
+            <th>Expire Date</th>
+            <th>Status</th>
+            <th>Notice</th>
         </tr>
-    <?php } ?>
-    </tbody>
-</table>
-<?php
+        </thead>
+        <tbody>
+        <?php foreach ($addons as $addon_slug => $addon) {
+            $license_sample = array(
+                'yatra-services' => array(
+                    'expire_date' => '2021-10-22',
+                    'license_key' => '',//'c7c40e988e2b3517eebc1a4fd5cf35fa',
+                    'status' => 'inactive',
+                    'notice' => __('Currently Active', 'yatra'),
+                    'id' => 12323,
 
-echo '<pre>';
-print_r($addons);
-echo '</pre>';
+                ));
+            $license = get_option('yatra_license', $license_sample);
+
+            $addon_license = isset($license_sample[$addon_slug]) ? $license_sample[$addon_slug] : array();
+
+            $display_license = isset($addon_license['license_key']) ? sanitize_text_field($addon_license['license_key']) : '';
+
+            $display_license = '' != $display_license ? '**********' . substr($display_license, 5, 10) : '';
+            ?>
+            <tr>
+                <td><span class="product-name"><?php echo esc_html($addon['label']) ?></span></td>
+                <td><?php
+                    if ($display_license === '') {
+                        ?>
+                        <input type="text" name="<?php echo esc_attr($addon_slug) ?>_license"
+                               placeholder="<?php echo __('Please enter your license key here', 'yatra') ?>"
+                               style="width: 100%"/>
+                        <?php
+                    } else {
+                        echo esc_html($display_license);
+                    }
+                    ?></td>
+                <td>
+
+                    <?php echo isset($addon_license['expire_date']) ? esc_html($addon_license['expire_date']) : '' ?>
+
+                </td>
+                <td>
+                    <span class="status <?php echo esc_attr(strtolower($addon_license['status'])) ?>"><?php echo isset($addon_license['status']) ? esc_html($addon_license['status']) : '' ?></span>
+                </td>
+                <td><?php echo isset($addon_license['notice']) ? esc_html($addon_license['notice']) : '' ?></td>
+
+
+            </tr>
+        <?php } ?>
+        </tbody>
+        <tfoot>
+        <tr>
+            <td colspan="5">
+                <button class="button-primary" type="submit"
+                        name="yatra_license_save_button"><?php echo __('Update', 'yatra'); ?></button>
+            </td>
+        </tr>
+
+        </tfoot>
+    </table>
+    <?php
+    wp_nonce_field('yatra_license_save_nonce');
+    ?>
+</form>
