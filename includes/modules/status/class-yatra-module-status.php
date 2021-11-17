04ef8581ce342e11bc59341b7a2730adc492bcd6
ughimire <me.umeshuser@gmail.com>
2021-11-17 21:42:44 +0545

    status currenttabs updated

--- includes/modules/status/class-yatra-module-status.php
+++ includes/modules/status/class-yatra-module-status.php
@@ -5,6 +5,8 @@ class Yatra_Module_Status
     public function __construct()
     {
         add_action('admin_menu', array($this, 'status_menu'));
+        add_action('yatra_status_system_status', array($this, 'system_status'));
+        add_action('yatra_status_logs', array($this, 'logs'));
     }
 
     public function status_menu()
@@ -23,7 +25,40 @@ class Yatra_Module_Status
     public function status()
     {
 
-        echo '<h1>Hello Status</h1>';
+        $current_tab = empty($_GET['tab']) ? '' : sanitize_title(wp_unslash($_GET['tab'])); // WPCS: input var okay, CSRF ok.
+
+        $current_section = empty($_REQUEST['section']) ? '' : sanitize_title(wp_unslash($_REQUEST['section'])); // WPCS: input var okay, CSRF ok.
+
+        $tabs = apply_filters('yatra_status_tabs_array', array(
+
+            'system_status' => __('System Status', 'yatra'),
+
+            'logs' => __('Logs', 'yatra')
+        ));
+
+        if ($current_tab === '' || !isset($tabs[$current_tab])) {
+
+            $tab_keys = array_keys($tabs);
+
+            $current_tab = $tab_keys[0];
+
+        }
+        include YATRA_ABSPATH . 'includes/modules/status/templates/html-admin-status.php';
+    }
+
+    public static function show_messages()
+    {
+
+    }
+
+    public function system_status()
+    {
+        echo '<h1>This is System Status Page</h1>';
+    }
+
+    public function logs()
+    {
+        echo '<h1>This is Log Page</h1>';
     }
 }
 
