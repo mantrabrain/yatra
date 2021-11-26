663aa944801cd9ff675892ddf58a16e13818927c
ughimire <me.umeshuser@gmail.com>
2021-11-26 08:48:00 +0545

    update

--- assets/admin/src/coupon/Counter.tsx
+++ assets/admin/src/coupon/Counter.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+
 export default class Counter extends React.Component {
     state = {
         count: 0
@@ -15,12 +16,12 @@ export default class Counter extends React.Component {
         });
     };
 
-    render () {
+    render() {
         return (
             <div>
                 <h1>{this.state.count}</h1>
-                <button onClick={this.increment}>Increment</button>
-                <button onClick={this.decrement}>Decrement</button>
+                <button type="button" onClick={this.increment}>Increment</button>
+                <button type="button" onClick={this.decrement}>Decrement</button>
             </div>
         );
     }
--- assets/admin/src/coupon/index.js
+++ assets/admin/src/coupon/index.js
@@ -1,12 +1,38 @@
 import * as React from 'react';
 import {render} from 'react-dom';
-
+import { TabPanel } from '@wordpress/components';
+import './style.css';
 import Counter from "./Counter";
 
+const onSelect = ( tabName ) => {
+    console.log( 'Selecting tab', tabName );
+};
+const MyTabPanel = () => (
+    <TabPanel
+        className="my-tab-panel"
+        activeClass="active-tab"
+        onSelect={ onSelect }
+        orientation="vertical"
+        tabs={ [
+            {
+                name: 'tab1',
+                title: 'Tab WOW',
+                className: 'tab-one',
+            },
+            {
+                name: 'tab2',
+                title: 'Tab 2',
+                className: 'tab-two',
+            },
+        ] }
+    >
+        { ( tab ) => <p>{ tab.title }</p> }
+    </TabPanel>
+);
 
 window.addEventListener("load", function () {
     render(
-        <Counter/>,
+        <MyTabPanel/>,
         document.getElementById("yatra-coupon-meta-element")
     );
 
