95f6dd306ede05a9d9f6c744a47c29ee08766b4f
ughimire <me.umeshuser@gmail.com>
2021-12-02 14:39:38 +0545

    pot file generated

--- includes/meta-boxes/class-yatra-metabox-coupons-cpt.php
+++ includes/meta-boxes/class-yatra-metabox-coupons-cpt.php
@@ -48,7 +48,7 @@ if (!class_exists('Yatra_Metabox_Coupons_CPT')) {
                     break;
                 case "usage_count":
                     $usage_limit = ($this->get_value('yatra_coupon_using_limit', $coupon_id));
-                    $usage_count_array = ($this->get_value('yatra_coupon_usages_bookings', $coupon_id));;
+                    $usage_count_array = ($this->get_value('yatra_coupon_usages_bookings', $coupon_id));
                     $usage_count_array = is_array($usage_count_array) ? $usage_count_array: array();
                     $usage_count = count($usage_count_array);
                     printf(
