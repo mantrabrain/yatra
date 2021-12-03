c5f217e9179d18b084e256669ca2e6d7f98a2c1c
ughimire <me.umeshuser@gmail.com>
2021-12-03 18:00:44 +0545

    make modular blocks

--- includes/class-yatra-modules.php
+++ includes/class-yatra-modules.php
@@ -6,6 +6,7 @@ class Yatra_Modules
     public static function render()
     {
 
+        include_once YATRA_ABSPATH . 'includes/modules/blocks/class-yatra-blocks.php';
         include_once YATRA_ABSPATH . 'includes/modules/dashboard/class-yatra-module-dashboard.php';
         include_once YATRA_ABSPATH . 'includes/modules/status/class-yatra-module-status.php';
 
--- includes/modules/blocks/class-yatra-blocks.php
+++ includes/modules/blocks/class-yatra-blocks.php
@@ -6,32 +6,25 @@ class Yatra_Blocks
     {
         add_action('init', array($this, 'register_blocks'));
         add_filter('block_categories_all', array($this, 'block_categories'), 10, 2);
-        add_action('wp_ajax_get_tours', array($this, 'get_tours'));
-    }
-
-    public function get_tours()
-    {
-
-        wp_send_json_success(array(
-            'tour_class' => "response_from_php-new updaed dynamic hhhhhhh sssssssssssss"
-        ));
     }
 
     public function register_blocks()
     {
         $block_dependency = file_exists(YATRA_ABSPATH . '/assets/build/blocks.asset.php') ? include_once(YATRA_ABSPATH . '/assets/build/blocks.asset.php') : array();
+
         $block_dependency['dependencies'] = isset($block_dependency['dependencies']) ? $block_dependency['dependencies'] : array();
+
         $block_dependency['version'] = isset($block_dependency['version']) ? sanitize_text_field($block_dependency['version']) : YATRA_VERSION;
+
         wp_register_script('yatra-blocks', YATRA_PLUGIN_URI . '/assets/build/blocks.js', $block_dependency['dependencies'], $block_dependency['version']);
 
         wp_localize_script('yatra-blocks', 'yatraBlocks', array(
             'ajax_url' => admin_url('admin-ajax.php'),
             'tour_action' => 'get_tours'
         ));
-        //wp_register_style('yatra-coupon-css', YATRA_PLUGIN_URI . '/assets/build/style-coupon.css', array('wp-components'), YATRA_VERSION);
-
 
         register_block_type('yatra/tour', array(
+
             'api_version' => 2,
 
             'editor_script' => 'yatra-blocks',
