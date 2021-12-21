acfb8dbf8ed24db52afab0fc85076d1c98154314
Umesh Ghimire <me.umeshuser@gmail.com>
2021-12-21 16:20:57 +0545

    update

--- includes/class-yatra-form-handler.php
+++ includes/class-yatra-form-handler.php
@@ -104,6 +104,7 @@ class Yatra_Form_Handler
 
         update_post_meta($booking_id, 'yatra_selected_payment_gateway', $payment_gateway_id);
 
+
         if ($booking_id > 0) {
 
             yatra_clear_session('yatra_tour_cart');
