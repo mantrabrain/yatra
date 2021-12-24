4fd1977da51fef9565e28a7af23f40e6c41d104e
ughimire <me.umeshuser@gmail.com>
2021-12-24 22:20:17 +0545

    update featured on list table

--- includes/hooks/yatra-list-table-hooks.php
+++ includes/hooks/yatra-list-table-hooks.php
@@ -2,56 +2,88 @@
 
 class Yatra_List_Table_Hooks
 {
-	public function __construct()
-	{
-		add_filter('manage_edit-tour_columns', array($this, 'add_new_columns'));
-		add_action('manage_tour_posts_custom_column', array($this, 'custom_columns'), 15, 2);
+    public function __construct()
+    {
+        add_filter('manage_edit-tour_columns', array($this, 'add_new_columns'));
+        add_action('manage_tour_posts_custom_column', array($this, 'custom_columns'), 15, 2);
 
-	}
-
-
-	function add_new_columns($columns)
-	{
-
-		unset($columns['taxonomy-attributes']);
-
-		array_splice($columns, 4, 0, array(
-			'attributes' => __('Attributes', 'yatra')
-		));
-		return $columns;
-
-	}
-
-	function custom_columns($column)
-	{
-		global $post;
-
-		$post_id = $post->ID;
-
-		$tour_meta_custom_attributes = get_post_meta($post_id, 'tour_meta_custom_attributes', true);
-
-		if (!is_array($tour_meta_custom_attributes)) {
-
-			$tour_meta_custom_attributes = array();
-		}
-
-		foreach ($tour_meta_custom_attributes as $term_id => $content) {
-
-			$term = get_term($term_id, 'attributes');
-
-			$term_content = $term->name;
-
-			if (isset($content['content'])) {
-				$term_content .= " : <strong>" . esc_html($content['content']) . '</strong>';
-			} else if ($content['shortcode']) {
-
-				$term_content .= " : <strong>" . esc_html($content['shortcode']) . '</strong>';
-			}
-			echo $term_content . '<br/>';
-		}
+    }
 
 
-	}
+    function add_new_columns($columns)
+    {
+
+        unset($columns['taxonomy-attributes']);
+        unset($columns['date']);
+
+        $columns['attributes'] = __('Attributes', 'yatra');
+        $columns['bookings'] = __('Bookings', 'yatra');
+        $columns['featured'] = __('Featured', 'yatra');
+        $columns['date'] = __('Date', 'yatra');
+
+        return $columns;
+
+    }
+
+    function custom_columns($column, $a)
+    {
+        global $post;
+
+        $post_id = $post->ID;
+
+        switch ($column) {
+            case "attributes":
+                $this->attributes($post_id);
+                break;
+            case "bookings":
+                $this->bookings($post_id);
+                break;
+            case "featured":
+                $this->featured($post_id);
+                break;
+        }
+
+
+    }
+
+    public function attributes($post_id)
+    {
+        $tour_meta_custom_attributes = get_post_meta($post_id, 'tour_meta_custom_attributes', true);
+
+        if (!is_array($tour_meta_custom_attributes)) {
+
+            $tour_meta_custom_attributes = array();
+        }
+
+        foreach ($tour_meta_custom_attributes as $term_id => $content) {
+
+            $term = get_term($term_id, 'attributes');
+
+            $term_content = $term->name;
+
+            if (isset($content['content'])) {
+                $term_content .= " : <strong>" . esc_html($content['content']) . '</strong>';
+            } else if ($content['shortcode']) {
+
+                $term_content .= " : <strong>" . esc_html($content['shortcode']) . '</strong>';
+            }
+            echo $term_content . '<br/>';
+        }
+    }
+
+    public function bookings($tour_id)
+    {
+
+    }
+
+    public function featured($tour_id)
+    {
+        $is_featured = (boolean)get_post_meta($tour_id, 'yatra_tour_meta_tour_featured', true);
+        $star_class = $is_featured ? 'star-filled' : 'star-empty';
+        ?>
+        <span class="yatra-featured-tour dashicons dashicons-<?php echo esc_attr($star_class) ?>"></span>
+        <?php
+    }
 }
 
 new Yatra_List_Table_Hooks();
