4b9ca911a9659757850da8cb233f472fdd20da94
ughimire <me.umeshuser@gmail.com>
2022-01-01 21:42:39 +0545

    npm run build

--- assets/build/js/block-tour.asset.php
+++ assets/build/js/block-tour.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '203bd77090afdb93703aa245f36eeddb');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'e704133bb07abc4adf045f97d9c4ff41');
