7d281f582a9a427dd04d1aaf82c9567d0e146136
Umesh Ghimire <me.umeshuser@gmail.com>
2022-01-07 16:17:32 +0545

    update

--- includes/admin/templates/metabox/booking/payment-status.php
+++ includes/admin/templates/metabox/booking/payment-status.php
@@ -6,14 +6,14 @@
     $all_payment_info = $payment->get_all_info(get_the_ID());
 
 
-    foreach ($all_payment_info as $info) {
+    foreach ($all_payment_info as $payment_id => $info) {
 
         $currency = $info['currency_code'];
 
         $currency_symbol = yatra_get_current_currency_symbol($currency);
         ?>
         <div class="yatra-payment-item">
-            <h2><?php echo esc_html($info['title']) ?></h2>
+            <h2><?php echo esc_html__('Payment - #') . absint($payment_id) ?></h2>
             <p class="flex">
                 <label for="yatra_payment_gateway"><strong>Payment Gateway: </strong></label>
                 <span><?php echo esc_html($info['payment_gateway']) ?></span>
