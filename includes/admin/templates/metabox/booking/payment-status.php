243844ea5d73893ef850de241fea1f7005ea5c66
ughimire <me.umeshuser@gmail.com>
2022-02-17 21:57:20 +0545

    transaction id added

--- includes/admin/templates/metabox/booking/payment-status.php
+++ includes/admin/templates/metabox/booking/payment-status.php
@@ -18,6 +18,10 @@
                 <span><?php echo esc_html($info['payment_gateway']) ?></span>
             </p>
             <p class="flex">
+                <label for="yatra_payment_gateway"><strong>Transaction ID: </strong></label>
+                <span><?php echo esc_html($info['transaction_id']) ?></span>
+            </p>
+            <p class="flex">
                 <label for="yatra_payment_gateway"><strong>Paid Amount: </strong></label>
                 <span><?php echo esc_html(yatra_get_price($currency_symbol, $info['paid_amount'])) ?></span>
             </p>
