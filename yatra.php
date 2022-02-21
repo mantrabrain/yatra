0de398380b95c4cfad938dbf4903a0586fdf6b90
ughimire <me.umeshuser@gmail.com>
2022-02-21 16:59:05 +0545

    version update

--- yatra.php
+++ yatra.php
@@ -22,7 +22,7 @@ if (!defined('YATRA_FILE')) {
 
 // Define YATRA_VERSION.
 if (!defined('YATRA_VERSION')) {
-    define('YATRA_VERSION', '2.1.7');
+    define('YATRA_VERSION', '2.1.8');
 }
 
 // Define YATRA_PLUGIN_URI.
