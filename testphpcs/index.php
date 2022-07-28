55a9cf30137db54fbca58a66e78b60fbb718c631
Umesh Ghimire <me.umeshuser@gmail.com>
2022-07-28 15:00:26 +0545

    update phpcs issue

--- testphpcs/index.php
+++ testphpcs/index.php
@@ -1,4 +1,19 @@
 <?php
+/**
+ * Gets the email message from the user's mailbox to add as
+ * a WordPress post. Mailbox connection information must be
+ * configured under Settings > Writing
+ *
+ * @package WordPress
+ */
+
+/**
+ * Prints signup_header via wp_head.
+ *
+ * @param int    $test_var Sample Variable.
+ * @param string $required_var any type of variable.
+ * @return string string return garcha.
+ */
 function yatra_wow( $test_var = 1, $required_var ) {
 	return 'Hello World' . $test_var . $required_var;
 }
