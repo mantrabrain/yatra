0d26f6ef8a5cd7235c98cc3268193ee40dc3c2eb
Umesh Ghimire <me.umeshuser@gmail.com>
2022-11-02 19:01:19 +0545

    update

--- includes/admin/templates/metabox/tour/pricing/add-new.php
+++ includes/admin/templates/metabox/tour/pricing/add-new.php
@@ -1,5 +1,9 @@
 <div class="yatra-field-wrap ">
     <label style="display: block;">&nbsp;&nbsp;</label>
-    <input class="button-primary" id="yatra_add_new_pricing_option" name="yatra_add_new_pricing_option" type="button"
-           value="<?php echo __('Add new pricing option', 'yatra') ?>">
+    <button class="button-primary" id="yatra_add_new_pricing_option" name="yatra_add_new_pricing_option" type="button"
+            value="<?php echo __('Add new pricing option', 'yatra') ?>">
+        <i class="dashicons dashicons-insert"></i>
+        <strong><?php echo esc_html(__('Add new pricing option', 'yatra')) ?></strong>
+
+    </button>
 </div>
