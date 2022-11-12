169653f24f301f668fd8a1bcc3994dd32574a68c
Umesh Ghimire <me.umeshuser@gmail.com>
2022-11-12 19:41:25 +0545

    design optimized

--- assets/build/js/block-activity.asset.php
+++ assets/build/js/block-activity.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '210b31eb9ef003ff4b5f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '6202e0b6c9cf7cf2b80f');
--- assets/build/js/block-tour.asset.php
+++ assets/build/js/block-tour.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'ba07db35ace3f275cf1d');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'e6345c4670b20acfd44a');
