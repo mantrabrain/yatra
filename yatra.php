c63aeb6553f99e3395a016877e67350c64ea79b0
Umesh Ghimire <me.umeshuser@gmail.com>
2025-09-11 18:19:14 +0545

    update

--- yatra.php
+++ yatra.php
@@ -22,7 +22,7 @@ if (!defined('YATRA_FILE')) {
 
 // Define YATRA_VERSION.
 if (!defined('YATRA_VERSION')) {
-    define('YATRA_VERSION', '2.3.0');
+    define('YATRA_VERSION', rand());
 }
 
 // Define YATRA_PLUGIN_URI.
